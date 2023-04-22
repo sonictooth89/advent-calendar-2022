a034797fd7c2edd9f587080f139ceca5ca812b35
Malgorzata Nowak <nowak.em@interia.eu>
2023-04-22 19:36:12 +0200

    exersise with calories

--- day1/app.js
+++ day1/app.js
@@ -4,8 +4,17 @@ readFile('./input.txt', 'utf-8', (err, data) => {
     if(err) {
         throw new Error(err);
     }
-    const transformData = '[' + data.replace(/\s/g, ',, ');
-    const dataArr = transformData.split(',, ,, ');
-    console.log(dataArr)
-});
+    const transformData = data.split("\n\n").map((row) => row.split("\n").map((value) => parseInt(value)));
 
+    let arrayOfSum = []; //tablica sum
+
+    for (let i = 0; i < transformData.length; i++) {
+        arrayOfSum.push(
+            transformData[i].reduce((prev, curr) => prev + curr)
+        );
+    };
+
+    const max = Math.max.apply(null, arrayOfSum);
+    console.log(`The most calories for Elf is: ${max}`);
+
+});
